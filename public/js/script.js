50f92d8b6629f25c0d66c55a8e6cfdfe105a1330
Laetitia Bernardi <33260laetitia.bernardi@gmail.com>
2018-08-16 07:39:08 +0200

    jquery page accueil et com

--- public/js/script.js
+++ public/js/script.js
@@ -29,3 +29,59 @@ $( "#closelegal" ).click(function() {
     // Animation complete.
   });
 });
+
+
+
+//faire apparaitre et disparaitre le menu en responsive
+  $(function() {
+    $('#show_menu').click(function() {
+      $('#mainmobil').first().show('slow', function showNextOne() {
+        $(this).next('#mainmobil').show('slow', showNextOne);
+      });    
+    });
+    $('#closemenu').click(function() {
+      $('#mainmobil').first().hide('slow', function hideNextOne() {
+        $(this).next('#mainmobil').hide('slow', hideNextOne);
+
+      });
+          
+    });
+  });
+
+//remonter du bas vers le haut de la page
+$(document).ready(function() {
+     $('a[href=#top]').click(function(){
+          $('html, body').animate({scrollTop:0}, 'slow');
+          return false;
+     });
+});
+
+
+//ancre vers l'acceuil
+$(document).ready(function() {
+     $('a[href=#navigation]').click(function(){
+          $('html, body').animate({scrollTop:$("#navigation").offset().top}, 'slow');
+          return false;
+     });
+});
+
+//ancre du bouton chapitre vers la liste des chapitres dans la page
+$(document).ready(function() {
+ 
+     $('a[href=#episodes]').click(function(){
+          $('html, body').animate({scrollTop:$("#episodes").offset().top}, 'slow');
+          return false;
+     });
+});
+
+//validation commentaire
+  var alert = $('#validCom');
+  if(alert.length>0){
+    alert.hide().animate({Top:$("#comSignal").offset().top});
+    alert.find('#closeCom').click(function(e){
+      e.preventDefault();
+      alert.slideUp();
+    })
+  }
+
+
