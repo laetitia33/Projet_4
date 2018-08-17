d7b2bedc1cc6d842f4b6a92b5ecc03fc00bb5ed0
Laetitia Bernardi <33260laetitia.bernardi@gmail.com>
2018-08-17 13:15:32 +0200

    userController

---
+++ controllers/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace V3\Controller;
+
+require_once('models/UserManager.php');
+
+use\Model\UserManager;
+use \Exception;
+
+class UserController
+{
+    private $_user;
+
+    public function __construct()
+    {
+        $this->_user = new UserManager();
+    }
+
+
+// Connexion
+    public function logUser($pseudo, $pass)
+    {
+        $user = $this->_user->getUser($pseudo);
+        $proper_pass = password_verify($_POST['pass'], $user['pass']);
+
+        if(!$user)
+        {
+            throw new Exception('Wrong username or/and password');
+        }
+      
+            elseif($proper_pass )
+            {
+                session_start();
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['pseudo'] = $user['pseudo'];
+                $_SESSION['pass'] = $user['pass'];
+
+                $id = $user['id'];
+                $pseudo = $user['pseudo'];
+                $pass_hash = $user['pass'];
+
+                setcookie('id', $id, time() + 1800, null, null, false, true);
+                setcookie('pseudo', $pseudo, time() + 1800, null, null, false, true);
+                setcookie('pass', $pass_hash, time() + 1800, null, null, false, true);
+
+                header('Location: index.php?action=administration');
+            }
+            else
+            {
+                throw new Exception('Wrong Username or Password');
+            }
+        }
+    
+
+
+// Deconnexion
+    public function logoutUser()
+    {
+        session_start();
+        $_SESSION = array();
+        session_destroy();
+
+        setcookie('id', '');
+        setcookie('pseudo', '');
+        setcookie('pass', '');
+    
+        header('Location: index.php');
+    }
+}
