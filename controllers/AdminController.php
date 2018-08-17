d497a5f71bc8cf7e228c2e4e41126f2b8bd7153e
Laetitia Bernardi <33260laetitia.bernardi@gmail.com>
2018-08-17 13:24:01 +0200

    admincontroller

---
+++ controllers/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Laetitia_Bernardi\projet4\Controller;
+
+require_once ('models/PostManager.php');
+require_once ('models/CommentManager.php');
+
+
+use \models\PostManager;
+use \models\CommentManager;
+
+
+class AdministrationController
+{
+    private $_post;
+    private $_comment;
+ 
+
+    public function __construct()
+    {
+        $this->_post = new \Laetitia_Bernardi\projet4\Model\PostManager();
+        $this->_comment = new \Laetitia_Bernardi\projet4\Model\CommentManager();
+      
+    }
+
+    public function administration()
+    {
+        $post = $this->_post->getLastPost();
+        $comment = $this->_comment->getLastComment();
+        $postsTotal = $this->_post->countPosts();
+        $commentsTotal = $this->_comment->countComments();
+        $commentsReportTotal = $this->_comment->countCommentsReport();
+    
+        require('views/adminView.php');
+    }
+}
